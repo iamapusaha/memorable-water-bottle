36ea776bfc0f803e0f1c78438a54647d438a6103
iamapusaha <apusaha392@gmail.com>
2023-09-10 22:05:20 +0600

    added css for data look better

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,8 +1,8 @@
-
+import './Bottle.css'
 const Bottle = ({ bottle }) => {
     const { name, price, img } = bottle;
     return (
-        <div>
+        <div className="bottle">
             <h2>{name}</h2>
             <img src={img} alt="" />
             <p>price: ${price}</p>
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
-
+import './Bottles.css'
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
     useEffect(() => {
@@ -12,7 +12,7 @@ const Bottles = () => {
     return (
         <div>
             <h1>Total Number Of Bottle: {bottles.length}</h1>
-            <div>
+            <div className="bottle-container">
                 {
                     bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
                 }
