6822f1714c76634248dcca10f5dd6c89d53f2911
iamapusaha <apusaha392@gmail.com>
2023-09-10 22:25:56 +0600

    add event handler for all product

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,12 @@
 import './Bottle.css'
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleAddToCart }) => {
     const { name, price, img } = bottle;
     return (
         <div className="bottle">
             <h2>{name}</h2>
             <img src={img} alt="" />
             <p>price: ${price}</p>
+            <button onClick={() => handleAddToCart(bottle)}>Purchase</button>
         </div>
     );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -4,17 +4,27 @@ import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
 const Bottles = () => {
     const [bottles, setBottles] = useState([]);
+    const [cart, setCart] = useState([])
     useEffect(() => {
         fetch('bottles.json')
             .then(res => res.json())
             .then(data => setBottles(data))
-    }, [])
+    }, []);
+    const handleAddToCart = (bottle) => {
+        const newCart = [...cart, bottle];
+        setCart(newCart)
+    }
     return (
         <div>
             <h1>Total Number Of Bottle: {bottles.length}</h1>
+            <h2>Available bottle:{cart.length}</h2>
             <div className="bottle-container">
                 {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                    bottles.map(bottle => <Bottle
+                        key={bottle.id}
+                        bottle={bottle}
+                        handleAddToCart={handleAddToCart}
+                    ></Bottle>)
                 }
             </div>
         </div>
